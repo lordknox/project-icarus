14a0ad73aaed52f04d6994bd484ac66f8d24de0c
Maalav Patadiya <156680012+lordknox@users.noreply.github.com>
2026-03-15 03:05:01 +0530

    feat: successfully tested the initial working prototpye of the website

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "tailwindcss";
 export default {
   darkMode: ["class"],
   content: [
-    "./client/index.html",
+    "./index.html",
     "./client/src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
