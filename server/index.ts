0980783b8087f803f5bcd439b757a5cfcfe6099a
Maalav Patadiya <156680012+lordknox@users.noreply.github.com>
2026-04-21 01:41:41 +0530

    Fixing CORS for production frontend

--- server/index.ts
+++ server/index.ts
@@ -13,12 +13,14 @@ const PORT = process.env.PORT || 4000;
 app.use(cors({
   origin: [
     'http://localhost:5173', // Local development
-    'https://project-icarus-q5au.onrender.com',
+    // 'https://project-icarus-q5au.onrender.com',
     'https://project-icarus-five.vercel.app',
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }));
+app.options('*', cors());
+
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
