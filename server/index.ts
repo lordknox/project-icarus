972fe7cbef80da1bf783575ba106c0af8a8f2b7d
Maalav Patadiya <156680012+lordknox@users.noreply.github.com>
2026-04-21 00:46:15 +0530

    Update CORS for production frontend

--- server/index.ts
+++ server/index.ts
@@ -6,9 +6,17 @@ import { setupRoutes} from './routes.js';
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+// app.use(cors({
+//     origin: process.env.CLIENT_URL || 'http://localhost:5173',
+//     credentials: true,
+// }));
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
-    credentials: true,
+  origin: [
+    'http://localhost:5173', // Local development
+    'https://project-icarus-q5au.onrender.com',
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }));
 // middleware
 app.use(express.json());
